a9a4ce76cb4cbd66b2397886b4d4a687bc550a76
prodrig911 <prodrig@ncsu.edu>
2019-07-19 02:52:58 -0400

    Commit: 7-18-2018

--- Project3/server.R
+++ Project3/server.R
@@ -4,8 +4,100 @@
 ## 7-17-2019
 
 library(shiny)
+library(tidyverse)
+library(DT)
 
 server <- function(input, output) {
+    
+    bbStats <- read_csv("C:/Users/Phillip/Desktop/ST 558/2018-2019 NBA.csv")
+    nba <- bbStats[-31,]
+    playoff <- data.frame(Playoff = ifelse(substring(nba$Team, 
+                                                     nchar(nba$Team)) == "*", "Y", "N"))
+    
+    conference <- matrix(c("E", "W", "W", "E", "W", "W", "W", "E", "W",
+                         "E", "W", "E", "W", "E", "E", "W", "W", "W", 
+                         "E", "W", "W", "E", "W", "E", "E", "E", "E", 
+                         "E", "E", "W"), nrow = 30, ncol = 1)
+    
+    colnames(conference) <- "Conference"
+    nba <- tbl_df(cbind(nba, playoff, conference))
+    nba$Team <- str_replace_all(nba$Team, "[[*]]", "")
+    
+    # nba$Team <- as.factor(nba$Team)
+    nba$Playoff <- as.factor(nba$Playoff)
+    nba$Conference <- as.factor(nba$Conference)
+    
+    getData <- reactive({
+        
+        team <- nba %>% 
+                select(Team,`FG%`,`3P`,`3P%`,`2P`,`2P%`,FT,`FT%`,ORB,
+                       DRB,AST,STL,BLK,TOV,PTS) %>% 
+                filter(Team == input$team)
+    })
+    
+    output$table1 <- renderDataTable({
+        
+        nba %>% select(Team,`FG%`,`3P`,`3P%`,`2P`,`2P%`,FT,`FT%`,ORB,
+                       DRB,AST,STL,BLK,TOV,PTS)
+    })
+    
+    output$table2 <- renderDataTable({
+        
+        getData()
+    })
+    
+    output$hist <- renderPlot({
+        
+        hist(nba$PTS, main = "Histogram of Points", xlab = "Points",
+             col = "cornflowerblue")
+        
+    })
+    
+    output$download1 <- downloadHandler(
+        
+        filename = function(){
+            
+            paste("2018-2019 NBA Statistics", ".csv", sep = "")
+        },
+        
+        content = function(file){
+            
+            write.csv(nba, file)
+        }
+        
+    )
+    
+    output$download2 <- downloadHandler(
+        
+        
+        filename = function(){
+            
+            paste("histogram", ".png", sep = "")
+        },
+        
+        content = function(file){
+            
+            png(file)
+            dev.off()
+        }
+        
+    )
+    
+    
+    output$download3 <- downloadHandler(
+            
+        filename = function(){
+
+            paste(input$team, ".csv", sep = "")
+        },
+
+        content = function(file){
+
+            write.csv(getData(), file)
+        }
+
+    )
+    
     
     
 }
--- Project3/ui.R
+++ Project3/ui.R
@@ -11,15 +11,36 @@ ui <- fluidPage(
     
     sidebarLayout(
         
-        sidebarPanel(),
+        sidebarPanel(
+            
+            conditionalPanel(condition = "input.tabs == '1'", h3("Welcome to My NBA App!")),
+            
+            conditionalPanel(condition = "input.tabs == '2'",
+                             downloadButton("download1", "Download Full Data")),
+            
+            conditionalPanel(condition = "input.tabs == '3'",
+                             conditionalPanel("input.tabs2 == '1'",
+                             downloadButton("download2", "Save Histogram"))),
+            
+            conditionalPanel(condition = "input.tabs == '4'",
+                             selectInput("team", "Team", choices = nba$Team),
+                             downloadButton("download3", "Download Team Data"))
+            
+        ),
         
         mainPanel(
-            tabsetPanel(type = "tabs",
-                        tabPanel("About", h2("About the App"), p("Some more text")),
-                        tabPanel("Data Exploration"),
-                        tabPanel("PCA"),
-                        tabPanel("Modeling"),
-                        tabPanel("Scroll through Data"))
+            tabsetPanel(type = "tabs", id = "tabs",
+                        tabPanel("About", value = "1", h2("About the App"), p("Some more text")),
+                        tabPanel("Scroll through Data", value = "2", dataTableOutput("table1"),
+                                 id = "tabselected"),
+                        tabPanel("Data Exploration", value = "3",
+                                 tabsetPanel(type = "tabs", id = "tabs2",
+                                             tabPanel("Histogram", plotOutput("hist"), value = '1')
+                                            )),
+                        tabPanel("Team Data", value = "4", dataTableOutput("table2"),
+                                 id = "tabselected"),
+                        tabPanel("PCA", value = "5"),
+                        tabPanel("Modeling", value = "6"))
         )
     )
 )
